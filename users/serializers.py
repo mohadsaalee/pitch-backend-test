707cf4c80b7d798a067beb5b2a6505a3d1d8c0b9
mohadsaalee <mohadsaalee@gmail.com>
2026-04-28 09:47:15 +0530

    fix login with email

--- users/serializers.py
+++ users/serializers.py
@@ -1,57 +1,30 @@
-from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate
 from rest_framework import serializers
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
-User = get_user_model()
-
-
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['email', 'full_name', 'role', 'password', 'password2']
-
-    def validate_role(self, value):
-        valid_roles = [r[0] for r in User.ROLE_CHOICES]
-        if not value:
-            raise serializers.ValidationError('Role is required.')
-        if value not in valid_roles:
-            raise serializers.ValidationError(f'Role must be one of: {", ".join(valid_roles)}')
-        return value
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({'password': 'Passwords do not match.'})
-        return attrs
-
-    def create(self, validated_data):
-        validated_data.pop('password2')
-        return User.objects.create_user(**validated_data)
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'email', 'full_name', 'role', 'email_verified', 'is_private_profile', 'created_at']
-        read_only_fields = ['id', 'email', 'email_verified', 'created_at']
-
-
-class ChangePasswordSerializer(serializers.Serializer):
-    old_password = serializers.CharField(write_only=True)
-    new_password = serializers.CharField(write_only=True, validators=[validate_password])
-    new_password2 = serializers.CharField(write_only=True)
-
-    def validate(self, attrs):
-        if attrs['new_password'] != attrs['new_password2']:
-            raise serializers.ValidationError({'new_password': 'Passwords do not match.'})
-        return attrs
-
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
-        data = super().validate(attrs)
-        data['user'] = UserSerializer(self.user).data
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        # 🔍 Find user by email
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("User not registered")
+
+        # 🔐 Authenticate using email as username
+        user = authenticate(username=user.email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError("Invalid password")
+
+        # 🔑 Generate tokens
+        refresh = self.get_token(user)
+
+        data = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+            "user": UserSerializer(user).data
+        }
+
         return data
